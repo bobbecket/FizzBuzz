06030f004fa758e4b8367f21cc7c85b160932721
Robert Becket <36114621+bobbecket@users.noreply.github.com>
2021-10-11 21:10:23 -0700

    Initial commit
    Initial commit

--- js/site.js
+++ js/site.js
@@ -1,3 +1,66 @@
-function HelloWorld(){
-    alert("Hello World");
+// Start / Controller function
+//
+// Get the fizz and buzz values from the Page
+function getValues() {
+
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
+
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
+
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+        let fbArray = fizzBuzz(fizzValue, buzzValue);
+        displayData(fbArray);
+    } else {
+        alert("Please enter integers");
+    }
+}
+
+// Logic functions
+//
+// Generate fizzBuzz array from 1 to 100
+function fizzBuzz(fizzValue, buzzValue) {
+
+    let fbArray = [];
+
+    for (let i = 1; i <= 100; i++) {
+        if (((i % fizzValue) == 0) && ((i % buzzValue) == 0)) {
+            fbArray.push("FizzBuzz");
+        } else if ((i % fizzValue) == 0) {
+            fbArray.push("Fizz");
+        } else if ((i % buzzValue) == 0) {
+            fbArray.push("Buzz");
+        } else {
+            fbArray.push(i);
+        }
+    }
+    return fbArray;
+}
+
+// Display / View function
+//
+// Display numbers and mark even numbers bold
+function displayData(fbArray) {
+
+    let tableBody = document.getElementById("results");
+    let templateRow = document.getElementById("fbTemplate");
+
+    tableBody.innerHTML = "";
+
+    let templateRows = "";
+    let numCols = 5;
+
+    for (let index = 0; index < fbArray.length; index += numCols) {
+
+        let tableRow = document.importNode(templateRow.content, true);
+
+        let rowCols = tableRow.querySelectorAll("td");
+        for (let j = 0; j < numCols; j++) {
+            rowCols[j].classList.add(fbArray[index + j]);
+            rowCols[j].textContent = fbArray[index + j];
+        }
+
+        tableBody.appendChild(tableRow);
+    }
 }
